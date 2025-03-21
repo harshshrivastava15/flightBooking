d3805488828220002e1ee85f83ce9a3fc58adebf
Harsh Shrivastava <harshshrivastava.work@gmail.com>
2025-03-21 19:43:55 +0530

    mapping done, now can access schedule as per id

--- src/main/java/com/flightBooking/flightBooking/controller/FlightScheduleController.java
+++ src/main/java/com/flightBooking/flightBooking/controller/FlightScheduleController.java
@@ -3,37 +3,29 @@ package com.flightBooking.flightBooking.controller;
 import com.flightBooking.flightBooking.services.FlightSchedule;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/flightSchedule")
 public class FlightScheduleController {
-    private FlightSchedule schedule = new FlightSchedule();
+    private Map<String, FlightSchedule> scheduleMap = new HashMap<>();
 
     @GetMapping
-    public FlightSchedule getAll(){
-        return schedule;
+    public Map getAll(){
+        return scheduleMap;
     }
 
 
     @PostMapping
-    public String createEntry(@RequestBody FlightSchedule testSchedule){
-        schedule.setId(testSchedule.getId());
-        schedule.setAirline(testSchedule.getAirline());
-        schedule.setDate(testSchedule.getDate());
-        schedule.setDestination(testSchedule.getDestination());
-        schedule.setDepartureTime(testSchedule.getDepartureTime());
-        schedule.setArrivalTime(testSchedule.getArrivalTime());
-
-        return "Schedule ho gaya";
-
+    public String createSchedule(@RequestBody FlightSchedule newSchedule) {
+        scheduleMap.put(newSchedule.getId(), newSchedule);
+        return "Schedule ho gaya!";
     }
 
     @GetMapping("id/{myId}")
     public FlightSchedule getScheduleById(@PathVariable String myId) {
-        if (schedule.getId() != null && schedule.getId().equals(myId)) {
-            return schedule;
-        } else {
-            return null;
-        }
+        return scheduleMap.get(myId);
     }
 
 }
