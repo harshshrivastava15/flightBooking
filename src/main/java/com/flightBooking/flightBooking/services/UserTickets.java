d0584c8d62ef49efa3d8e4ff1ce2df39153ebcec
Harsh Shrivastava <harshshrivastava.work@gmail.com>
2025-03-21 19:47:43 +0530

    created parameters for user tickets

---
+++ src/main/java/com/flightBooking/flightBooking/services/UserTickets.java
@@ -0,0 +1,43 @@
+package com.flightBooking.flightBooking.services;
+
+public class UserTickets {
+    private String ticketId;
+    private String passengerName;
+    private String flightId;
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(String flightId) {
+        this.flightId = flightId;
+    }
+
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    private String seatNumber;
+
+
+}
