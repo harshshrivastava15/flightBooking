805aa11434ac1bc35fc32a32b3383df3173d4e55
Harsh Shrivastava <harshshrivastava.work@gmail.com>
2025-03-21 19:39:54 +0530

    test of flight schedule done now adding map in it to get result as per id

--- src/main/java/com/flightBooking/flightBooking/controller/FlightScheduleController.java
+++ src/main/java/com/flightBooking/flightBooking/controller/FlightScheduleController.java
@@ -1,4 +1,39 @@
 package com.flightBooking.flightBooking.controller;
 
+import com.flightBooking.flightBooking.services.FlightSchedule;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/flightSchedule")
 public class FlightScheduleController {
+    private FlightSchedule schedule = new FlightSchedule();
+
+    @GetMapping
+    public FlightSchedule getAll(){
+        return schedule;
+    }
+
+
+    @PostMapping
+    public String createEntry(@RequestBody FlightSchedule testSchedule){
+        schedule.setId(testSchedule.getId());
+        schedule.setAirline(testSchedule.getAirline());
+        schedule.setDate(testSchedule.getDate());
+        schedule.setDestination(testSchedule.getDestination());
+        schedule.setDepartureTime(testSchedule.getDepartureTime());
+        schedule.setArrivalTime(testSchedule.getArrivalTime());
+
+        return "Schedule ho gaya";
+
+    }
+
+    @GetMapping("id/{myId}")
+    public FlightSchedule getScheduleById(@PathVariable String myId) {
+        if (schedule.getId() != null && schedule.getId().equals(myId)) {
+            return schedule;
+        } else {
+            return null;
+        }
+    }
+
 }
--- src/main/java/com/flightBooking/flightBooking/controller/Test.java
+++ src/main/java/com/flightBooking/flightBooking/controller/Test.java
@@ -1,4 +1,4 @@
-package com.flightBooking.flightBooking.Controller;
+package com.flightBooking.flightBooking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
